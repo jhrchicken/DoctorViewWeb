5cd46c50b922678175d6572be7718f39caab2dcd
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2024-09-23 10:19:44 +0900

    Feat: 댓글 DTO 추가

---
+++ src/main/java/com/edu/springboot/board/CommentDTO.java
@@ -0,0 +1,14 @@
+package com.edu.springboot.board;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class CommentDTO {
+	private int comm_idx;
+	private Date postdate;
+	private String content;
+	private int board_ref;
+	private String writer_ref;
+}
--- src/main/java/com/edu/springboot/board/IFreeboardService.java
+++ src/main/java/com/edu/springboot/board/IFreeboardService.java
@@ -7,18 +7,20 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface IFreeboardService {
-	// 목록: 자유게시판의 게시글 개수를 카운트
+	// 게시글 목록: 자유게시판의 게시글 개수를 카운트
 	public int countPost(ParameterDTO parameterDTO);
-	// 목록: 자유게시판에서 한 페이지에 출력할 게시글을 인출
+	// 게시글 목록: 자유게시판에서 한 페이지에 출력할 게시글을 인출
 	public ArrayList<BoardDTO> listPost(ParameterDTO parameterDTO);
-	// 조회
+	// 게시글 조회
 	public BoardDTO viewPost(BoardDTO boardDTO);
-	// 조회: 자유게시판에서 게시글의 조회수 증가
+	// 게시글 조회: 자유게시판에서 게시글의 조회수 증가
 	public int plusVisitcount(BoardDTO boardDTO);
-	// 작성
+	// 게시글 작성
 	public int writePost(String title, String content, String writer_idx);
-	// 수정
+	// 게시글 수정
 	public int editPost(BoardDTO boardDTO);
-	// 삭제
+	// 게시글 삭제
 	public int deletePost(String idx);
+	// 댓글 조회
+	public CommentDTO viewComment(CommentDTO commentDTO);
 }
