41d0be50fefd5c7aa8d2cde416161b219ed3f898
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2024-09-26 19:22:10 +0900

    Merge branch 'main' of https://github.com/jhrchicken/DoctorViewWeb.git into main

--- src/main/resources/static/js/ui-common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // #header.on
-  $('#header .gnb>li>a').on('mouseenter', function() {
-    $('#header').addClass('on');
-  });
-  $('#header').on('mouseleave', function() {
-    $('#header').removeClass('on');
-  });
-});
