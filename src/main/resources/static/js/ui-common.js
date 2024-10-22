51ca7a794dd3f6d9a26bafb5083ff2347873c8cd
leeeeeeeeeeeehr <jysg2908123@gmail.com>
2024-10-22 09:35:47 +0900

    Fix: 스와이퍼 에러 해결

--- src/main/resources/static/js/ui-common.js
+++ src/main/resources/static/js/ui-common.js
@@ -7,25 +7,31 @@ document.addEventListener("DOMContentLoaded", function () {
     $('#header').removeClass('on');
   });
   
-  // 스와이퍼
-  var swiperTag = new Swiper('.tag .swiper', {
-    loop: true,
-    slidesPerView: 6,  
-    spaceBetween: 20,
-    centeredSlides: false,
-  });
+  // 메인 페이지의 스와이퍼
+  if (document.querySelector('.tag .swiper')) {
+	  var swiperTag = new Swiper('.tag .swiper', {
+	    loop: true,
+	    slidesPerView: 6,  
+	    spaceBetween: 20,
+	    centeredSlides: false,
+	  });
+  }
   
-  // 스와이퍼
-  var swiperDoctor = new Swiper('.doctor .swiper', {
-    loop: false,
-    slidesPerView: 3,  
-    spaceBetween: 20,
-    centeredSlides: false,
-  });
-  var swiperTime = new Swiper('.time .swiper', {
-	loop: false,
-	slidesPerView: 4,  
-    spaceBetween: 20,
-    centeredSlides: false,
-  });
+  // 병원 상세페이지의 스와이퍼
+  if (document.querySelector('.doctor .swiper')) {	
+	  var swiperDoctor = new Swiper('.doctor .swiper', {
+	    loop: false,
+	    slidesPerView: 3,  
+	    spaceBetween: 20,
+	    centeredSlides: false,
+	  });
+  }
+  if (document.querySelector('.time .swiper')) {	
+	  var swiperTime = new Swiper('.time .swiper', {
+		loop: false,
+		slidesPerView: 4,  
+	    spaceBetween: 20,
+	    centeredSlides: false,
+	  });
+  }
 });
